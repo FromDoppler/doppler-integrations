c5c35dd0f762524a0cbe3778651cc88532175d48
LucasPelizza <lpelizza@makingsense.com>
2018-03-15 11:44:49 -0300

    [NoTicket] Add logic to support radio buttons in the Unbounce's form

---
+++ Doppler.Integrations/Mapper/FieldTypes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace Doppler.Integrations.Mapper
+{
+    enum FieldTypes
+    {
+        [DescriptionAttribute("boolean")]
+        Boolean = 1,
+        [DescriptionAttribute("string")]
+        String = 2,
+        [DescriptionAttribute("date")]
+        Date = 3
+    };
+
+    static class EnumExtensions
+    {
+        public static string Description(this Enum value)
+        {
+            var enumType = value.GetType();
+            var field = enumType.GetField(value.ToString());
+            var attributes = field.GetCustomAttributes(typeof(DescriptionAttribute), false).Cast<object>().ToArray();
+
+            return attributes.Length == 0 ? value.ToString() : ((DescriptionAttribute)attributes[0]).Description;
+        }
+    }
+}
--- Doppler.Integrations/Mapper/MapperSubscriber.cs
+++ Doppler.Integrations/Mapper/MapperSubscriber.cs
@@ -7,7 +7,7 @@ using Doppler.Integrations.Models.Dtos;
 namespace Doppler.Integrations.Mapper
 {
     /// <inheritdoc/>
-    public class MapperSubscriber: IMapperSubscriber
+    public class MapperSubscriber : IMapperSubscriber
     {
         private readonly ILogger _log;
 
@@ -19,29 +19,26 @@ namespace Doppler.Integrations.Mapper
         /// <inheritdoc/>
         public DopplerSubscriberDto ToDopplerSubscriberDto(IDictionary<string, IList<object>> rawSubscriber, ItemsDto allowedFields)
         {
-            var email = string.Empty;
-            if (rawSubscriber.ContainsKey("email"))
-            {
-                email = rawSubscriber["email"][0].ToString();
-                rawSubscriber.Remove("email");
-            }
-            else
-            {
-                _log.LogWarning("The current user has not included an EMAIL field");
-            }
-
+            var email = GetEmailValue(rawSubscriber);
             var fieldsUpperNameAllowed = allowedFields.Items.Select(i => i.Name.ToUpper()).ToList();
             var fieldsNameAllowed = allowedFields.Items.Select(i => i.Name).ToList();
-
             var fields = new List<CustomeFieldDto>();
             var fieldsNotEnabled = new List<string>();
 
             foreach (KeyValuePair<string, IList<object>> entry in rawSubscriber)
             {
                 var index = fieldsUpperNameAllowed.IndexOf(entry.Key.ToUpper());
-                if ( index >= 0 )
+                if (index >= 0)
                 {
-                    fields.Add(new CustomeFieldDto { Name = fieldsNameAllowed[index], Value = entry.Value[0] });
+                    var type = allowedFields.Items[index].Type;
+                    var value = entry.Value[0].ToString();
+                    if (type == FieldTypes.Boolean.Description())
+                    {
+                        value = GetBooleanValue(value);
+                    }
+
+                    var newCustomeField = new CustomeFieldDto { Name = fieldsNameAllowed[index], Value = value };
+                    fields.Add(newCustomeField);
                 }
                 else
                 {
@@ -51,9 +48,7 @@ namespace Doppler.Integrations.Mapper
 
             if (fieldsNotEnabled.Count > 0)
             {
-                var warningFields = fieldsNotEnabled.Aggregate((current, next) => current + ", " + next);
-                var warningDescription = string.Format("The following fields have been rejected because they are not allowed on Doppler's subscriber: $0", warningFields);
-                _log.LogWarning(warningDescription);
+                LogFieldsRejected(fieldsNotEnabled);
             }
 
             return new DopplerSubscriberDto
@@ -61,6 +56,50 @@ namespace Doppler.Integrations.Mapper
                 Email = email,
                 Fields = fields
             };
+        }
+
+        private string GetBooleanValue(string value)
+        {
+            var valueTranslated = value;
+            if (value == "Si" || value == "SI" || value == "si" || value == "True" || value == "TRUE")
+            {
+                valueTranslated = "true";
+            }
+            else if (value == "No" || value == "NO" || value == "no" || value == "False" || value == "FALSE")
+            {
+                valueTranslated = "false";
+            }
+
+            return valueTranslated;
+        }
+
+        private string GetEmailValue(IDictionary<string, IList<object>> rawSubscriber)
+        {
+            var email = string.Empty;
+
+            if (rawSubscriber.ContainsKey("email"))
+            {
+                email = rawSubscriber["email"][0].ToString();
+                rawSubscriber.Remove("email");
+            }
+            else if (rawSubscriber.ContainsKey("EMAIL"))
+            {
+                email = rawSubscriber["EMAIL"][0].ToString();
+                rawSubscriber.Remove("EMAIL");
+            }
+            else
+            {
+                _log.LogWarning("The current user has not included an EMAIL field");
+            }
+
+            return email;
+        }
+
+        private void LogFieldsRejected(List<string> valuesList)
+        {
+            var warningFields = valuesList.Aggregate((current, next) => current + ", " + next);
+            var warningDescription = string.Format("The following fields have been rejected because they are not allowed on Doppler's subscriber: $0", warningFields);
+            _log.LogWarning(warningDescription);
         }
     }
 }
