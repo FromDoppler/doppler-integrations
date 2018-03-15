0e6b2eb00d162f375001ad5599a6335da1d28965
lpelizzaMS <lpelizza@makingsense.com>
2018-03-15 12:37:05 -0300

    [NoTicket] Add logic to support radio buttons in the Unbounce's form

---
+++ Doppler.Integrations/Mapper/FieldTypes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace Doppler.Integrations.Mapper
+{
+    enum FieldTypes
+    {
+        [DescriptionAttribute("boolean")]
+        Boolean = 1,
+        [DescriptionAttribute("string")]
+        String = 2,
+        [DescriptionAttribute("date")]
+        Date = 3
+    };
+
+    static class EnumExtensions
+    {
+        public static string GetDescription(this FieldTypes value)
+        {
+            var enumType = typeof(FieldTypes);
+            var field = enumType.GetField(value.ToString());
+            var attribute = field.GetCustomAttribute<DescriptionAttribute>();
+
+            return attribute?.Description ?? value.ToString();
+        }
+    }
+}
